e5adcb4263b469c93984f135317fa17c5f56e8e8
Adam Olshansky <adam.olshansky@sv.cmu.edu>
2012-09-29 21:50:06 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NflSite::Application.config.secret_token = '0e79d398d0cc1d8302da034dc7b5a0c25916f308754e251ab7ec0893508ad852ee88e6b252d68d3ff05562ed3bf41027fd9a9467fbf10085890980e87bcdd886'
